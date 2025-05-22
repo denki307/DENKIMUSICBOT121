7cd7f66f65c9f83b0ea642980ccb5734fa9d6d98
denki307 <mbtdenki@gmail.com>
2025-05-22 16:24:28 +0530

    Update config.py
    Signed-off-by: denki307 <mbtdenki@gmail.com>

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/xbitcode/music.git",
+    "https://github.com/denki307/DENKIMUSICBOT121",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
